b63c96b1987644de80460dfdcbf79d95ca5518d6
Rodrygo <simrodri10@gmail.com>
2025-09-05 12:17:44 -0400

    clase5 - CREACION DE CONTROLADORES

---
+++ src/main/java/com/app1/springboot_aplicacion/SpringbootAplicacionApplication.java
@@ -0,0 +1,13 @@
+package com.app1.springboot_aplicacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAplicacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAplicacionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app1/springboot_aplicacion/SpringbootAplicacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app1.springboot_aplicacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAplicacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
