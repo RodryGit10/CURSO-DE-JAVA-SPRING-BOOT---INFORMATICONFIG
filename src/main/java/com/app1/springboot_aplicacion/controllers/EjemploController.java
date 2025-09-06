3e84cca555a823c647a534f4cc1b8b6cb4d3b273
Rodrygo <simrodri10@gmail.com>
2025-09-05 23:27:59 -0400

    clase6 - PASAR DATOS A LA VISTA

--- src/main/java/com/app1/springboot_aplicacion/controllers/EjemploController.java
+++ src/main/java/com/app1/springboot_aplicacion/controllers/EjemploController.java
@@ -1,6 +1,7 @@
 package com.app1.springboot_aplicacion.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -9,7 +10,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class EjemploController {
 
     @GetMapping("/detalles_info")
-    public String info() {
+    public String info(Model model) {
+        model.addAttribute("Titulo", "Servidor en Linea 👍😊");
+        model.addAttribute("Servidor", "RodrigoSpring 🙌");
+        model.addAttribute("Ip", "192.168.1.1 👌");
         return "detalles_info";
     }
 
