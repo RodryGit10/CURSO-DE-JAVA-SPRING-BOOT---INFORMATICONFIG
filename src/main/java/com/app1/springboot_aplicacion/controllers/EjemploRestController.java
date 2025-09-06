49bdf95da3756e60eb3e817518ce23d1a65fefa9
Rodrygo <simrodri10@gmail.com>
2025-09-06 18:07:27 -0400

    clase10 CLASES DTO

--- src/main/java/com/app1/springboot_aplicacion/controllers/EjemploRestController.java
+++ src/main/java/com/app1/springboot_aplicacion/controllers/EjemploRestController.java
@@ -1,26 +1,22 @@
 package com.app1.springboot_aplicacion.controllers;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.app1.springboot_aplicacion.models.Empleados;
+import com.app1.springboot_aplicacion.models.dto.ClaseDTO;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@RestController  //Indica que es API
 @RequestMapping("/api")
 public class EjemploRestController {
 
     @GetMapping(path = "/detalles_info2")
-    public Map<String, Object>  detalles_info2(){  //Map: Es una estructura de datos que almacena pares clave-valor. String: El tipo de las claves (siempre serán textos) Object: El tipo de los valores (pueden ser cualquier tipo de objeto). detalles_info2: Nombre del método
-        Empleados empleado1 = new Empleados("Rodrygo", "Carita", "Mecapaca Nro.2154", 
-                                            "Gerente", 29, 71242948, 001);
-        Map<String, Object> respuesta = new HashMap<>();  //crea una instancia de una mapa. En este caso HashMap
-        respuesta.put("Empleado", empleado1); //put: Agrega o actualiza un par clave-valor en el mapa
-        return respuesta;  // Spring convierte esto a JSON automáticamente 
+    public ClaseDTO detalles_info(){  //Metodo -> detalles_info de clase ClaseDTO
+        ClaseDTO usuario1 = new ClaseDTO(); //Instanciacion
+        usuario1.setTitulo("Administrador");
+        usuario1.setUsuario("Rodry.sim");
+        return usuario1;
     }
 
 }
---
+++ src/main/java/com/app1/springboot_aplicacion/models/dto/ClaseDTO.java
@@ -0,0 +1,24 @@
+package com.app1.springboot_aplicacion.models.dto;
+
+public class ClaseDTO {
+
+    private String titulo, usuario;  //Atributos
+
+    //Setters y Getters
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+}
