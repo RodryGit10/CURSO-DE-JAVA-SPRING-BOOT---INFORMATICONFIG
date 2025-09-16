dea7170854d3ad8301c31ef26f2124bdf9a8097b
Rodrygo <simrodri10@gmail.com>
2025-09-15 20:31:13 -0400

    clase12 - DIRECTIVAS DE THYMELEAF

--- src/main/java/com/app1/springboot_aplicacion/controllers/EjemploController.java
+++ src/main/java/com/app1/springboot_aplicacion/controllers/EjemploController.java
@@ -14,7 +14,7 @@ public class EjemploController {
     @GetMapping("/detalles_info")
     public String info(Model model) {  // instaciamos "Model", poniendo como nombre "model"
         //Instanciamos el modelo para acceder a sus atributos
-        Empleados empleado1 = new Empleados("Rodrygo", "Carita", "Mecapaca Nro.2154", 
+        Empleados empleado1 = new Empleados("Rodrygo", "Carita", null,
                                             "Gerente", 29, 71242948, 001);
         model.addAttribute("Empleado", empleado1);
         return "detalles_info";
